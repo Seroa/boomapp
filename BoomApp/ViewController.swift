a14cc4864f4b2c22f07284b25e23e1f4bfe33762
Serhat Kavi <Seroa@Serhat-MacBook-Pro.local>
2016-02-08 00:53:33 +0200

    Initial Commit

---
+++ BoomApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoomApp
+//
+//  Created by Serhat Kavi on 8.02.2016.
+//  Copyright (c) 2016 Serhat Kavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
